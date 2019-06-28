46752581220092de539c94cc9542d3b797c09f0a
castchise <feelmoredrama@gmail.com>
2019-06-28 22:28:59 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div>
+    <div class="container mx-auto text-center text-2xl">
+      <h1>Мои инструменты</h1>
+      <div class="py-2 bg-gray-500">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius incidunt eveniet natus dolorum nemo! Ipsum quo delectus iure vitae eum quam magnam aperiam eos recusandae? Eveniet officia corporis placeat eius illo atque odit libero expedita dolore ut consectetur fugit architecto provident doloribus labore sint eligendi soluta, deserunt animi eos recusandae ea quod dignissimos. Natus est aliquam veniam, adipisci deleniti sequi similique nostrum sapiente aut asperiores quo ratione ab quod voluptate excepturi nisi. Obcaecati vero, quis odit dolore veniam ea cumque aut eos distinctio unde quisquam facilis placeat doloremque, consequuntur assumenda necessitatibus reprehenderit minus qui laboriosam fugiat itaque adipisci quibusdam similique.
+      </div>
+    </div>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import './assets/main.css';
+// import card from './components/Card';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    // card
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
